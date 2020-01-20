e70e951907b3d6d9cbcd31749ec979ab8325db21
Matt Burke <spraints@gmail.com>
2020-01-20 14:25:06 -0500

    get started with an example
    https://github.com/spockbots/birdhumming/blob/52df87f32bec13542014a0e6716e13eafcd6b865/main-motor.py

--- line_follower/main.py
+++ line_follower/main.py
@@ -7,6 +7,41 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+import time
 
 # Write your program here
-brick.sound.beep()
+brick.sound.beeps(1)
+#brick.sound.beep(60)
+#brick.sound.beep(1000, 1000, 50)
+leftmotor = Motor (Port.B)
+rightmotor = Motor (Port.C)
+
+diameter = 56
+axletrack = 114
+
+#robot = DriveBase (left, right, diameter, axletrack)
+
+
+#robot.drive(200, 0.5)
+
+def forward(speed,distance):
+    circumferance = diameter * math.pi
+    angle = (distance * 360) / circumferance
+    leftmotor.run_angle(speed,angle,Stop.COAST,False)
+    rightmotor.run_angle(speed,angle,Stop.COAST,False)
+
+def left(speed, angle):
+    leftmotor.run_angle(speed,-angle,Stop.COAST,False)
+    rightmotor.run_angle(speed,angle,Stop.COAST,False)
+    time.sleep(1)
+
+def right(speed, angle):
+    rightmotor.run_angle(speed,-angle,Stop.COAST,False)
+    leftmotor.run_angle(speed,angle,Stop.COAST,False)
+    time.sleep(1)
+
+#forward(1000,500)
+left (1000, 180)
+
+right(1000, 180)
+time.sleep(3)
