94f80766c8e756d45baf89abfad991db353be0cd
Matt Burke <spraints@gmail.com>
2020-01-20 14:33:31 -0500

    Use DriveBase

--- line_follower/main.py
+++ line_follower/main.py
@@ -11,37 +11,14 @@ import time
 
 # Write your program here
 brick.sound.beeps(1)
-#brick.sound.beep(60)
-#brick.sound.beep(1000, 1000, 50)
+
 leftmotor = Motor (Port.B)
 rightmotor = Motor (Port.C)
 
-diameter = 56
-axletrack = 114
+diameter = 43 # mm
+axletrack = 145 # mm
+robot = DriveBase (leftmotor, rightmotor, diameter, axletrack)
 
-#robot = DriveBase (left, right, diameter, axletrack)
-
-
-#robot.drive(200, 0.5)
-
-def forward(speed,distance):
-    circumferance = diameter * math.pi
-    angle = (distance * 360) / circumferance
-    leftmotor.run_angle(speed,angle,Stop.COAST,False)
-    rightmotor.run_angle(speed,angle,Stop.COAST,False)
-
-def left(speed, angle):
-    leftmotor.run_angle(speed,-angle,Stop.COAST,False)
-    rightmotor.run_angle(speed,angle,Stop.COAST,False)
-    time.sleep(1)
-
-def right(speed, angle):
-    rightmotor.run_angle(speed,-angle,Stop.COAST,False)
-    leftmotor.run_angle(speed,angle,Stop.COAST,False)
-    time.sleep(1)
-
-#forward(1000,500)
-left (1000, 180)
-
-right(1000, 180)
-time.sleep(3)
+robot.drive(100, 0)
+time.sleep(2)
+robot.stop()
