6a39af2696f2404ddba63c63f9a90d2e6e38b216
Matt Burke <spraints@gmail.com>
2020-01-20 15:06:32 -0500

    a little better
    this handles gentle turns pretty well, but not right angles.

--- line_follower/main.py
+++ line_follower/main.py
@@ -15,29 +15,37 @@ brick.sound.beeps(1)
 leftmotor = Motor(Port.B)
 rightmotor = Motor(Port.C)
 
-diameter = 43 # mm
-axletrack = 145 # mm
-robot = DriveBase(leftmotor, rightmotor, diameter, axletrack)
+# diameter = 43 # mm
+# axletrack = 145 # mm
+# robot = DriveBase(leftmotor, rightmotor, diameter, axletrack)
 
 c = ColorSensor(Port.S3)
 t = TouchSensor(Port.S1)
 
-SPEED = 75 # mm / second
+SPEED = 150 # deg / second
 MIDPOINT = 50
-CORRECTION = -2.5
+CORRECTION = -5
 INTERVAL = 0.05 # seconds
 
 while not t.pressed():
   cv = c.reflection()
   mismatch = cv - MIDPOINT
   direction = mismatch * CORRECTION
-  if direction > 75:
-    direction = 75
-  if direction < -75:
-    direction = -75
+  if direction > SPEED:
+    direction = SPEED
+  if direction < -SPEED:
+    direction = -SPEED
 
   print("Reflect: ", cv, ", Mismatch = ", mismatch, " -> Direction: ", direction)
   
-  robot.drive(SPEED, direction)
+  if direction < 0:
+    leftmotor.run(SPEED)
+    rightmotor.run(SPEED + direction)
+  else:
+    leftmotor.run(SPEED - direction)
+    rightmotor.run(SPEED)
 
   time.sleep(INTERVAL)
+
+leftmotor.stop()
+rightmotor.stop()
